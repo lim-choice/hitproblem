e3a6aa8e86f488d3e2df96080656667e82a067bc
limchoice <1mchoice@naver.com>
2025-03-12 23:09:46 +0900

    진행중인 시험 취소 물어보는 모달 프로퍼티 수정

--- client/src/components/Exams/ExamContinueModal.tsx
+++ client/src/components/Exams/ExamContinueModal.tsx
@@ -20,6 +20,9 @@ const ExamContinueModal: React.FC<ExamContinueModalProps> = ({
       onCancel={onCancel}
       okText="시험 이어서 하기"
       cancelText="진행중인 시험 취소"
+      maskClosable={false} //모달 바깥쪽을 클릭해도 닫히지 않는 프로퍼티
+      keyboard={false} //esc키 (X)키를 눌러도 닫히지 않는 프로퍼티
+      closable={false} //우측상단 X키를 숨기는 프로퍼티
       centered
     >
       <p>진행 중인 시험이 있습니다. 이어서 진행하시겠습니까?</p>
